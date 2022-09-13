a319ba177bb8570d089b119c58b44e8fabc8dcb6
JoseSoloaga <josesoloagam@gmail.com>
2022-09-13 11:42:52 +0200

    parece que funciona

---
+++ server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+//#### ENDPOINTS USER #####
+
+//registro de user
+
+//login user
+
+//devuelve info del user
+
+
+//##### ENDPOINTS RECOMENDACIONES #####
+
+//lista de las recomendaciones
+
+//nueva recomendacion
+
+//devuelve una recomendacion
+
+//borrar recomendacion
+
+
+// Middlewares notFound y error
+app.use((req, res) => {
+    res.status(404).send({
+        status: 'Error',
+        message: 'Not found',
+    });
+});
+
+app.use((error, req, res, next) => {
+    console.error(error);
+
+    res.status(error.httpStatus || 500).send({
+        status: 'Error',
+        message: error.message,
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server listening at: localhost://3000');
+});
