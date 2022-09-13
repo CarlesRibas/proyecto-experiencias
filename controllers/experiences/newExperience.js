b5438b75c415b2a7fa876c497c6f234ab4e793a5
AlexDiaz <xandre88@gmail.com>
2022-09-13 18:25:12 +0200

    experiencias añadidas

--- controllers/experiences/newExperience.js
+++ controllers/experiences/newExperience.js
@@ -8,34 +8,26 @@ const newExperience = async (req, res, next) => {
     try {
         connection = await getDB();
 
-        // Recuperar los datos del body de la request
-        const { title, entry, text } = req.body;
+        const { title, entry, text, place } = req.body;
 
-        // Validar los datos que recibimos usando el squema
-        await validate(newExperienceSchema, req.body);
+        /*  await validate(newExperienceSchema, req.body); */
 
-        // Necesitamos el id del usuario para asignarselo al producto
-        // como propietario del mismo
         const idReqUser = req.userAuth.id;
 
-        // Ya comprobamos que name y price son obligatorios en el schema
-
-        // Si falta alguno de los campos obligatorios damos un error
         if (!title || !text || !entry) {
             throw generateError('Debes indicar los campos obligatorios', 400);
         }
 
-        // Si nos inidica el nombre y precio, insertamos el nuevo producto
         const [{ insertId }] = await connection.query(
-            `INSERT INTO recomendaciones (title, entry, text, createdAt, idUser)
-            VALUES (?, ?, ?, ?, ?)`,
-            [title, entry, text, new Date(), idReqUser]
+            `INSERT INTO recomendaciones (title, entry, text, created_at, users_id, place)
+            VALUES (?, ?, ?, ?, ?, ?)`,
+            [title, entry, text, new Date(), idReqUser, place]
         );
 
         res.send({
             status: 'Ok',
-            message: 'Experiencia insertado con éxito!',
-            data: { id: insertId, title, entry, text },
+            message: 'Experiencia insertada con éxito!',
+            data: { id: insertId, title, entry, text, place },
         });
     } catch (error) {
         next(error);
--- controllers/users/loginUser.js
+++ controllers/users/loginUser.js
@@ -1,6 +1,6 @@
+const getDB = require('../../db/getDB');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { getDB } = require('../../db/getDB');
 const { generateError } = require('../../helpers');
 require('dotenv').config();
 
@@ -8,52 +8,41 @@ const loginUser = async (req, res, next) => {
     let connection;
 
     try {
-        // establecemos una conexion a la base de datos
-        connection = await getDB;
+        connection = await getDB();
 
-        // Obtener los email y password del body
         const { email, password } = req.body;
 
         if (!email || !password) {
-            throw generateError('Faltan campos obligatorios', 400); // Bad Request
+            throw generateError('Faltan campos obligatorios', 400);
         }
 
-        // Comprobamos que existe un usuario con ese email en la base de datos
         const [user] = await connection.query(
             `SELECT id, email, password FROM users WHERE email = ?`,
             [email]
         );
+        console.log(user);
 
-        // Si no existe un usuario con ese email? Lanzamos un error
         if (user.length < 1) {
             throw generateError(
                 'No existe un usuario registrado con ese email',
                 404
-            ); // Not found
+            );
         }
-
-        // Si existe un usuario con ese email comprobamos que las contraseñas coinciden
 
         const validPassword = await bcrypt.compare(password, user[0].password);
 
-        // Si no coinciden las contraseñas damos un error
         if (!validPassword) {
             throw generateError('La contraseña es incorrecta', 401); // Unauthorized
         }
 
-        // Si el usuario indica un email y contraseña correctos, generaremos un token de inicio de sesion
-
-        // Crear un objeto con la informacion que pasaremos al token
         const tokenInfo = {
             id: user[0].id,
         };
 
-        // Crear el token
         const token = jwt.sign(tokenInfo, process.env.SECRET, {
             expiresIn: '10d',
         });
 
-        // Enviamos de respuesta el token generado
         res.send({
             status: 'Ok',
             authToken: token,
@@ -61,7 +50,7 @@ const loginUser = async (req, res, next) => {
     } catch (error) {
         next(error);
     } finally {
-        if(connection)connection.release();
+        if (connection) connection.release();
     }
 };
 
