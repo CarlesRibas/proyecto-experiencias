bb9c3baaffbc88950132bb522fa96d04c1885e82
JoseSoloaga <josesoloagam@gmail.com>
2022-09-13 13:34:57 +0200

    loginUser

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const morgan = require('morgan');
+const loginUser = require('./controllers/users/loginUser');
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 
+
+
+
 //#### ENDPOINTS USER #####
 
 //registro de user
 
 //login user
+app.post('/login', loginUser)
 
 //devuelve info del user
 
